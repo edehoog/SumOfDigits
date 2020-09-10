d327458664f1b2a0f5cc82c023621f58abfcba0d
Elija de Hoog <elija.dehoog@gmail.com>
2020-09-10 00:48:38 -0700

    git_review - Experimental branch add even

--- SumOfDigits/Program.cs
+++ SumOfDigits/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Runtime.InteropServices;
 
 namespace SumOfDigits
 {
@@ -9,12 +10,21 @@ namespace SumOfDigits
             Console.Write("Input  a number(integer): ");
             int n = Convert.ToInt32(Console.ReadLine());
             int sum = 0;
+            int evenSum = 0;
+            int count = 0;
             while (n != 0)
             {
                 sum += n % 10;
+                if (count % 2 != 0)
+                {
+
+                    evenSum += n % 10;
+                }
                 n /= 10;
+                count++;
             }
             Console.WriteLine("Sum of the digits you entered is: " + sum);
+            Console.WriteLine("Sum of the digits with even positions is: " + evenSum);
             Console.WriteLine("Thank you");
 
         }
